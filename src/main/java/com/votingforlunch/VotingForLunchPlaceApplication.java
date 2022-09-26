027add89d73d8fc3adf17bcc9ce6c5c07c611010
Anastasia Leonteva <anastasia.leonteva21@gmail.com>
2022-09-26 21:37:27 +0300

    init app

---
+++ src/main/java/com/votingforlunch/VotingForLunchPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.votingforlunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingForLunchPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingForLunchPlaceApplication.class, args);
+        System.out.println();
+    }
+}
---
+++ src/test/java/com/votingforlunch/VotingForLunchPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votingforlunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingForLunchPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
